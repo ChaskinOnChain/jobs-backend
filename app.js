5e6f4edb679a39a69a6ded23ad82ec3c9f807ebc
Jason Chaskin <jason.chaskin@gmail.com>
2023-02-05 16:13:16 -0500

    added security middleware

--- app.js
+++ app.js
@@ -6,7 +6,24 @@ const app = express()
 const userRouter = require("./routes/users-routes")
 const jobsRouter = require("./routes/jobs-routes")
 const authorizeUser = require("./middleware/authenticate")
+const helmet = require("helmet")
+const xss = require("xss-clean")
+const cors = require("cors")
+const rateLimiter = require("express-rate-limit")
 
+app.set("trust proxy", 1)
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    })
+)
+
+app.use(helmet())
+app.use(xss())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
